090bd69ab1b8c6e1c37b3273cc8abe0a2080ed97
Ashish Kumar <ashish.kumar@zopper.com>
2024-04-20 10:42:26 +0530

    convereted whole package into typescript

--- index.js
+++
@@ -1,4 +0,0 @@
-const debounce = require("./Debounce/Debounce");
-const throttle = require("./Throttling/Throttling");
-
-module.exports = { debounce, throttle };
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+
+export * from "./debounce-throttle";
