1f76b4cd7f974f3919fc07aeac74d62263b77d14
Ashish Kumar <ashish.kumar@zopper.com>
2024-05-11 11:25:12 +0530

    auto- build updated

--- src/text-tweak.ts
+++ src/text-tweak.ts
@@ -151,9 +151,65 @@ function toReverseCase(str: string) {
 function toSmallCaps(str: string) {
   if (!str) throw new Error("String cannot be empty");
   if (typeof str !== "string") throw new TypeError("String must be a string");
+  const smallCapsMap: { [key: string]: string } = {
+    A: "ᴀ",
+    B: "ʙ",
+    C: "ᴄ",
+    D: "ᴅ",
+    E: "ᴇ",
+    F: "ғ",
+    G: "ɢ",
+    H: "ʜ",
+    I: "ɪ",
+    J: "ᴊ",
+    K: "ᴋ",
+    L: "ʟ",
+    M: "ᴍ",
+    N: "ɴ",
+    O: "ᴏ",
+    P: "ᴘ",
+    Q: "ǫ",
+    R: "ʀ",
+    S: "s",
+    T: "ᴛ",
+    U: "ᴜ",
+    V: "ᴠ",
+    W: "ᴡ",
+    X: "x",
+    Y: "ʏ",
+    Z: "ᴢ",
+    a: "ᴀ",
+    b: "ʙ",
+    c: "ᴄ",
+    d: "ᴅ",
+    e: "ᴇ",
+    f: "ғ",
+    g: "ɢ",
+    h: "ʜ",
+    i: "ɪ",
+    j: "ᴊ",
+    k: "ᴋ",
+    l: "ʟ",
+    m: "ᴍ",
+    n: "ɴ",
+    o: "ᴏ",
+    p: "ᴘ",
+    q: "ǫ",
+    r: "ʀ",
+    s: "s",
+    t: "ᴛ",
+    u: "ᴜ",
+    v: "ᴠ",
+    w: "ᴡ",
+    x: "x",
+    y: "ʏ",
+    z: "ᴢ",
+    " ": " ",
+  };
+
   return str
     .split("")
-    .map((char) => (char === char.toUpperCase() ? char.toLowerCase() : char))
+    .map((char) => smallCapsMap[char] || char)
     .join("");
 }
 
